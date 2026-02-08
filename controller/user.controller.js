48baaa87053f869744ff0cf2776b278770ca2188
whynot-byoul <whynot.byoul@gmail.com>
2026-02-09 00:24:34 +0900

    에러메세지 보내는 것으로 수정

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -42,7 +42,7 @@ UserController.loginWithEmail = async (req, res) => {
       throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
     }
   } catch (error) {
-    res.status(400).json({ status: "fail", error });
+    res.status(400).json({ status: "fail", error: error.message });
   }
 };
 
